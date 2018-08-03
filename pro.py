d1c1eb0655dba5fd9800b54d16f82079b31fbf5e
Swayam Shah <swayamshah97@gmail.com>
2018-08-03 13:29:51 +0530

    Update pro.py

--- pro.py
+++ pro.py
@@ -99,4 +99,4 @@ def storing_file(filename):
 if __name__ == "__main__":
     app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
-    app.run(port="0.0.0.0",debug=True)
+    app.run(host='0.0.0.0',port='5000',debug=True)
